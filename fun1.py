0be80a245f14f0762bb12cfb7c589b96f9002550
raheeq19-meet <raheeq19@meet.mit.edu>
2017-07-27 12:13:32 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,15 @@
+def add_numbers(start,end):
+    c =0
+    for number in range(start,end+1):
+        #print(number)
+        c = c+ number
+    return c
+
+
+
+test1=add_numbers(1,2)
+print(test1)
+test2=add_numbers(1,100)
+print(test2)
+test3=add_numbers(333,777)
+print(test3)
