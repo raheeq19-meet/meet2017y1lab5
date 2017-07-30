5c6c9c06c794b19f4e04e22c35abf976cc05270c
raheeq19-meet <raheeq19@meet.mit.edu>
2017-07-30 12:55:06 +0200

    Stopping here

---
+++ fun2.py
@@ -0,0 +1,18 @@
+#def draw_1d(n):
+   # print("*"*n)
+
+#def draw_1d(n,c):
+    #print(c*n)
+
+#def draw_2d(n,m,c):
+    #for number in range(m):
+        #draw_1d(n,c)
+
+
+   
+    
+    
+    
+    
+    
+    
